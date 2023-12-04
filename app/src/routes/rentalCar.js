f32e28f6c51347edfbfa6e97251b0f3b230ebfb3
ella00100 <ella00100@github.com>
2023-12-04 16:45:55 +0900

    fix : rentButton -> rentalCar and return에 통합

--- app/src/routes/rentalCar.js
+++ app/src/routes/rentalCar.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { insertSql, selectSql } from "../database/sql";
-
+import { insertSql} from "../database/sql";
+import { updateSql } from "../database/sql";
+import { exec } from "child_process";
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -8,5 +9,23 @@ router.get('/', (req, res) => {
     res.render('rentalCar', { userId: userId });
     console.log(userId);
 });
+
+router.post('/startRental', async (req, res) => {
+    try {
+        console.log('Start Rental Request Received:', req.body);
+        const { customerID, carName } = req.body;
+        const result = await insertSql.startRental({ CustomerID: customerID, CarName: carName });
+        console.log('Rental started:', result);
+
+        // Redirect to the "return" page after a successful rental
+        res.redirect('/return');
+    } catch (error) {
+        console.error('Error starting rental:', error);
+        res.status(500).send('Error starting rental: ' + error.message);
+    }
+});
+
+
+export default router;
 
 module.exports = router;
--- app/src/routes/return.js
+++ app/src/routes/return.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { updateSql } from "../database/sql";
+import { exec } from "child_process";
 
 const router = express.Router();
 
@@ -6,4 +8,34 @@ router.get('/', (req, res) => {
     res.render('return');
 });
 
+
+// "return" 버튼 클릭 시 호출될 라우트
+router.post('/', async (req, res) => {
+    try {
+        console.log('End Rental Request:', req.body);
+        const { rentalID } = req.body;
+
+        // 대여 종료 시간을 업데이트
+        await updateSql.endRental(rentalID);
+
+        // DriveList 테이블의 RentalTime 업데이트 (임의의 값 60으로 고정)
+        await updateSql.updateDriveListRentalTime(rentalID, 60); // 60 분으로 고정
+
+        console.log('Rental successfully ended and DriveList updated');
+        res.send('Rental ended and DriveList updated successfully');
+
+        // 파이썬 스크립트 실행
+        exec('model\\zigzag.py', (error, stdout, stderr) => {
+            if (error) {
+                console.error('Error executing Python script:', error);
+            } else {
+                console.log('Python script executed successfully');
+                console.log('Python script output:', stdout);
+            }
+        });
+    } catch (error) {
+        console.error('Error in endRental:', error);
+        res.status(500).send('Error ending rental and updating DriveList: ' + error.message);
+    }
+});
 module.exports = router;
